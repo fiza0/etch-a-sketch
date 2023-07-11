bde5f2c7d7aef5c5363b73791663ada1596f8949
fiza0 <issafuad05@gmail.com>
2023-07-11 11:08:55 +0300

    Make button that prompts user input
    input value is not validated,however

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 let gridDimension
 let backgroundColor
 
-while(true){
+dimensionButton=document.createElement('button')
+dimensionButton.textContent='how many squares per a side?'
+selections=document.querySelector('.selections')
+selections.appendChild(dimensionButton)
+
+dimensionButton.addEventListener('click',()=>{
     gridDimension=prompt('how many squares per a side?')
-    if(gridDimension>100){
-        alert('too large!')
-        continue
-    }else if(gridDimension<=0){
-        alert('invalid')
-    }else break
-}
-for(i=0;i<gridDimension**2;i++){
-    gridDiv=document.createElement('div')
-    gridDiv.classList.add('grid-div')
-    gridDivDimensions=(100/gridDimension)
-    gridDiv.style.height=`${gridDivDimensions}%`
-    gridDiv.style.width=gridDiv.style.height
-    sketchZone=document.querySelector('.sketch-zone')
-    sketchZone.appendChild(gridDiv)
-}
-gridDivs=document.querySelectorAll('.grid-div')
-gridDivs.forEach(gridDiv=>{
-    gridDiv.addEventListener('mouseover',()=>{
-        backgroundColor='black'
-        gridDiv.style.backgroundColor=`${backgroundColor}`
+    for(i=0;i<gridDimension**2;i++){
+        gridDiv=document.createElement('div')
+        gridDiv.classList.add('grid-div')
+        gridDivDimensions=(100/gridDimension)
+        gridDiv.style.height=`${gridDivDimensions}%`
+        gridDiv.style.width=gridDiv.style.height
+        sketchZone=document.querySelector('.sketch-zone')
+        sketchZone.appendChild(gridDiv)
+    }
+    gridDivs=document.querySelectorAll('.grid-div')
+    gridDivs.forEach(gridDiv=>{
+        gridDiv.addEventListener('mouseover',()=>{
+            backgroundColor='black'
+            gridDiv.style.backgroundColor=`${backgroundColor}`
+                
             
-        
+        })
     })
+
 })
+
