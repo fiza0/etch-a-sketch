fa331251f37d8b674dd861e1ab39bbbfda0af566
fiza0 <issafuad05@gmail.com>
2023-07-11 12:59:37 +0300

    Add comments

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ selections.appendChild(dimensionButton)
 dimensionButton.addEventListener('click',()=>{
     let gridDiv
     let gridDivDimensions
+    //clears canvas before creating specified number of divs
     let gridDivs=document.querySelectorAll('.grid-div')
     gridDivs.forEach(gridDiv=>{
         gridDiv.remove()
     })
+    //validation:ensures input is a number and is between 0 and 100
     while(true){
         gridDimension=prompt('how many squares per a side?')
         if(gridDimension>100){
@@ -23,6 +25,10 @@ dimensionButton.addEventListener('click',()=>{
             continue
         }else break
     }
+    /*creates as many divs as the user specified(user_input^2) and assigns them their class.
+    the divs are then given dimensions to ensure that there is a uniform number of divs per
+    a row . the divs are then appended to the sketch zone. the divs are later made to wrap 
+    into rows using flex box*/
     for(i=0;i<gridDimension**2;i++){
         gridDiv=document.createElement('div')
         gridDiv.classList.add('grid-div')
@@ -32,6 +38,8 @@ dimensionButton.addEventListener('click',()=>{
         sketchZone=document.querySelector('.sketch-zone')
         sketchZone.appendChild(gridDiv)
     }
+    /*loops over each div in the sketch zone and adds an event listener that changes its color
+    to black when it is hovered on */
     gridDivs=document.querySelectorAll('.grid-div')
     gridDivs.forEach(gridDiv=>{
         gridDiv.addEventListener('mouseover',()=>{
