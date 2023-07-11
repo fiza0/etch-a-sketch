8bd472d1586c0d606b120596c92181286538108c
fiza0 <issafuad05@gmail.com>
2023-07-11 09:36:07 +0300

    Make divs appear as a grid

---
+++ script.js
@@ -0,0 +1,10 @@
+gridDimension=16
+for(i=0;i<gridDimension**2;i++){
+    gridDiv=document.createElement('div')
+    gridDiv.classList.add('grid-div')
+    gridDivDimensions=(100/gridDimension)
+    gridDiv.style.height=`${gridDivDimensions}%`
+    gridDiv.style.width=gridDiv.style.height
+    sketchZone=document.querySelector('.sketch-zone')
+    sketchZone.appendChild(gridDiv)
+}
